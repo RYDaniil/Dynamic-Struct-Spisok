08d8c68be256caf185ed02bfc155619ce2d5b0af
RYDaniil <79157317+RYDaniil@users.noreply.github.com>
2021-06-07 20:27:20 +0200

    Add files via upload

---
+++ LabList/Elem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabList
+{
+    class Elem<T>
+    {
+        private object data;
+        public Elem<object> pnext;
+        public Elem<object> ppred;
+
+        public Elem()
+        {
+
+        }
+
+        public Elem(object newData)
+        {
+            data = newData;
+            pnext = null;
+            ppred = null;
+        }
+
+        public object Data {
+            get
+            {
+                return data;
+            }
+
+            set
+            {
+                data = value;
+            }
+        }
+        public object DataReturn
+        {
+            get
+            {
+                return (T)data;
+            }
+        }
+
+    }
+}
---
+++ LabList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LabList
+{
+    class Program
+    {
+        static Elem<object> Add(double data, Elem<object> predElem)
+        {
+            var newElem = new Elem<object>(data);
+            predElem.pnext = newElem;
+            newElem.ppred = predElem;
+            return newElem;
+        }
+
+        static void Main(string[] args)
+        {
+            var head = new Elem<object>();
+            var elem = head;
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                elem = Add(Convert.ToDouble(Console.ReadLine()), elem);
+            }
+
+            head.ppred = elem;
+            elem.pnext = head;
+
+            Console.WriteLine("");
+            elem = head.pnext;
+            while (elem.Data != null)
+            {
+                Console.WriteLine(elem.Data);
+                elem = elem.pnext;
+            }
+            Console.WriteLine("");
+
+
+            elem = head.pnext;
+            while (elem.Data != null)
+            {
+                if (Convert.ToDouble(elem.Data) < 0)
+                {
+                    if (elem.pnext.Data == null)
+                    {
+                        var elem1 = elem.pnext.pnext;
+                        elem.pnext.pnext = elem1.pnext;
+                        elem1.pnext.ppred = elem.pnext;
+                    }
+                    else
+                    {
+                        var elem1 = elem.pnext;
+                        elem.pnext = elem1.pnext;
+                        elem1.pnext.ppred = elem;
+                    }
+                }
+                elem = elem.pnext;
+            }
+
+
+            elem = head.pnext;
+            while (elem.Data != null)
+            {
+                Console.WriteLine(elem.Data);
+                elem = elem.pnext;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
